26341d02d74071d276ad59558d219c639f16f940
owy <126146587+goldietiara@users.noreply.github.com>
2024-06-13 20:58:13 +0700

    adding object, lightning and orbit controls

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 import "./style.css";
 import * as THREE from "three";
 // import javascriptLogo from './javascript.svg'
@@ -24,3 +25,50 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight); // make it full screen
 camera.position.setZ(30);
 renderer.render(scene, camera);
+
+///// ADDING OBJECT
+//GEOMETRY {the x,y,z points that makeup a shape}
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+//MATERIAL like the wrapping paper for an object
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff6347,
+});
+
+//MESH by combining geometry and material
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+//LIGHTNING
+
+//Point = light based on position
+const pointLight = new THREE.PointLight(0xffffff); // 0x = hexadecimal literal
+pointLight.position.set(5, 5, 5);
+
+//Ambient = light everything in the scene equaly
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+//Helper = showing the position of the light
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//GRID
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
